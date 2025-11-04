3bf253508a0b4402f617980940be489cb8ebe600
spark_repo- <reddyuttej4@gmail.com>
2025-11-04 15:05:31 +0530

    supabase connection

--- database.py
+++ database.py
@@ -1,13 +1,9 @@
-# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# # DATABASE_URL = "postgresql://postgres:1234@localhost:5432/HMDB"  
-# # DATABASE_URL="postgresql://postgres:[Nexnora14@]@db.ccfynwuswlcbcldkjvdk.supabase.co:5432/postgres"
-# DATABASE_URL="postgresql://postgres:[Nexnora14@]@db.ccfynwuswlcbcldkjvdk.supabase.co:5432/postgres"
+# Supabase PostgreSQL connection string
 DATABASE_URL = "postgresql://postgres:uttejreddy2025@db.ccfynwuswlcbcldkjvdk.supabase.co:5432/postgres"
-
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
